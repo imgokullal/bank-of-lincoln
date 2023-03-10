19206eddacfd1ea5a56de03348a157e9843cb073
gokullal <gokullal29112000@gmail.com>
2023-03-10 19:40:02 +0000

    starting php

---
+++ register.php
@@ -0,0 +1,35 @@
+<?php
+include('./components/header.php');
+?>
+
+<div class="content">
+  <div class="form">
+    <form action="">
+      <div class="form-element">
+        <label for="">Name</label><br />
+        <input type="text" class="form-label" />
+      </div>
+      <div class="form-element">
+        <label for="">Date of Birth</label><br />
+        <input type="date" class="form-label" />
+      </div>
+      <div class="form-element">
+        <label for="">Username</label><br />
+        <input type="text" class="form-label" />
+      </div>
+      <div class="form-element">
+        <label for="">Email id</label><br />
+        <input type="text" class="form-label" />
+      </div>
+      <div class="form-element">
+        <label for="">Password</label><br />
+        <input type="password" class="form-label" />
+      </div>
+      <input type="button" value="Register" class="button button1">
+      <a href="" class="button button2">Login</a>
+    </form>
+  </div>
+</div>
+</body>
+
+</html>
